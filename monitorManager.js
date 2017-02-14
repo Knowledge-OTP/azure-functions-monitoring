0efdb2cad8e0ed6453ddc3d6c84b772474db6bea
assafhp <assafshp@zahav.net.il>
2017-02-14 19:07:15 +0200

    remove console

--- monitorManager.js
+++ monitorManager.js
@@ -1,24 +1,74 @@
 var _monitorManager;
+const FUNCTIONS_ARRAY_KEY='functions_array_key';
 
 var Monitoring = function (redisInfo) {
-    this.monitorData={};
     this.redisInfo=redisInfo;
-};
+    this.monitorData={};
+    if (redisInfo){
+        var redis = require("redis");
+        var redisName = redisInfo.host + '.redis.cache.windows.net';
+        var redisKey = redisInfo.key;
+        this.redisClient = redis.createClient(6380,redisName, {auth_pass: redisKey, tls: {servername: redisName}});
+        this.redisClient.get(FUNCTIONS_ARRAY_KEY,  function(err, reply) {
+            if (err){
+                //err
+            }
+            else{
+                if (reply){
+                    this.monitorData=JSON.parse(reply);
+                }
+                 
+            }
+            
+        });
+    }
+}
 
 
+Monitoring.prototype._setMonitoringData = function(monitoringDataObj){
+
+    this.monitorData[monitoringDataObj.function_name] = monitoringDataObj;
+    if (this.redisInfo){
+        this.redisClient.set(FUNCTIONS_ARRAY_KEY, JSON.stringify(this.monitorData),function(err, reply) {
+            //
+        });
+    }
+}
+
+Monitoring.prototype.clear = function (){
+
+    this.redisClient.del(FUNCTIONS_ARRAY_KEY,function(err, reply) {
+        //    
+    });
+    
+}
 
 Monitoring.prototype.log = function (functionName,invokedTime,isSuceess){
 
-    this.monitorData[functionName] = {
+    var monitoringDataObj = {
         function_name: functionName,
         last_invoke: invokedTime,
         suceess: isSuceess
     }
+
+    this._setMonitoringData(monitoringDataObj);
+    
 }
 
-Monitoring.prototype.get = function (){
-    
+Monitoring.prototype.get = function (callback){
     var nowTime = new Date().getTime();
+    
+    if (this.redisInfo){
+        this.redisClient.get(FUNCTIONS_ARRAY_KEY,  function(err, reply) {
+            if (err){
+                // 
+            }
+            else{
+                this.monitorData = JSON.parse(reply);
+            }
+        });
+    }
+
     for(var functionName in this.monitorData) {
         var minutesFromLastInvoke = (nowTime - this.monitorData[functionName].last_invoke)/1000/60;
         this.monitorData[functionName].monitorTime= nowTime;
@@ -27,11 +77,9 @@ Monitoring.prototype.get = function (){
     return (this.monitorData);
 }
 
-var monitorManager = function(){
+var monitorManager = function(redisInfo){
     if (!_monitorManager){
-        _monitorManager = new Monitoring();
+        _monitorManager = new Monitoring(redisInfo);
     }
     return _monitorManager; 
 };
-
-module.exports = monitorManager;
