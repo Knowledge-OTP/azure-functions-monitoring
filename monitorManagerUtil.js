1dfeb833222b84482267f3e235142e98b7e505b1
assafhp <assafshp@zahav.net.il>
2017-02-14 19:10:38 +0200

    added redis

--- monitorManagerUtil.js
+++ monitorManagerUtil.js
@@ -3,10 +3,10 @@
 	var MonitorManager = require('./monitorManager.js');
 	const path = require('path');
 
-	var logFunctionExecution = function(azureContext, dirname, isSucess){
+	var logFunctionExecution = function(azureContext, dirname, isSucess, redisInfo){
 	
 		var timeStamp = new Date().getTime();
-	    var monitorManager = MonitorManager();   
+	    var monitorManager = MonitorManager(redisInfo);   
 	    var functionName = path.basename(dirname);
 
 	    azureContext.log(functionName + ' was triggered !!!');
