69113ed4de174ee19a914eda295a98f8b16c053a
assafhp <assafshp@zahav.net.il>
2017-02-15 18:58:21 +0200

    fixes

--- monitorManager.js
+++ monitorManager.js
@@ -1,11 +1,13 @@
 var _monitorManager;
 const FUNCTIONS_ARRAY_KEY='functions_array_key';
+var extend = require('extend');
+
 
 var Monitoring = function (redisInfo) {
     this.redisInfo=redisInfo;
     this.monitorData={};
     if (redisInfo){
-        // console.log('redis');
+        var monitorData = this.monitorData;
         var redis = require("redis");
         var redisName = redisInfo.host + '.redis.cache.windows.net';
         var redisKey = redisInfo.key;
@@ -16,12 +18,9 @@ var Monitoring = function (redisInfo) {
             }
             else{
                 if (reply){
-                    this.monitorData=JSON.parse(reply);
-                    // console.log('reply,monitorData'+ JSON.stringify(this.monitorData));
+                    monitorData=JSON.parse(reply);
                 }
-                 
             }
-            
         });
     }
 }
@@ -29,12 +28,31 @@ var Monitoring = function (redisInfo) {
 
 Monitoring.prototype._setMonitoringData = function(monitoringDataObj){
 
-    this.monitorData[monitoringDataObj.function_name] = monitoringDataObj;
     if (this.redisInfo){
-        this.redisClient.set(FUNCTIONS_ARRAY_KEY, JSON.stringify(this.monitorData),function(err, reply) {
-            //
+        var monitorData = this.monitorData;
+        var client = this.redisClient;
+        client.get(FUNCTIONS_ARRAY_KEY,  function(err, reply) {
+            if (err){
+                // 
+            }
+            else{
+                var redisMonitorData = JSON.parse(reply);
+                extend(monitorData,redisMonitorData);
+                console.log('monitorData=' + monitorData);
+                monitorData[monitoringDataObj.function_name] = monitoringDataObj;
+                client.set(FUNCTIONS_ARRAY_KEY, JSON.stringify(monitorData),function(err, reply) {
+                    //
+                });
+               
+            }
         });
     }
+    else{
+        this.monitorData[monitoringDataObj.function_name] = monitoringDataObj;
+    }
+    // console.log('end monitorData=' + monitorData);
+    console.log('this.monitorData=' + this.monitorData);
+
 }
 
 Monitoring.prototype.clear = function (){
@@ -59,21 +77,23 @@ Monitoring.prototype.log = function (functionName,invokedTime,isSuceess){
 
 Monitoring.prototype.get = function (callback){
     var nowTime = new Date().getTime();
+
     
     if (this.redisInfo){
+        var monitorData = this.monitorData;
         this.redisClient.get(FUNCTIONS_ARRAY_KEY,  function(err, reply) {
             if (err){
                 // 
                 callback({ err: true, data: err});
             }
             else{
-                this.monitorData = JSON.parse(reply);
-                for(var functionName in this.monitorData) {
-                    var minutesFromLastInvoke = (nowTime - this.monitorData[functionName].last_invoke)/1000/60;
-                    this.monitorData[functionName].monitorTime= nowTime;
-                    this.monitorData[functionName].minutesFromLastInvoke=minutesFromLastInvoke;
+                extend(monitorData,JSON.parse(reply));
+                for(var functionName in monitorData) {
+                    var minutesFromLastInvoke = (nowTime - monitorData[functionName].last_invoke)/1000/60;
+                    monitorData[functionName].monitorTime= nowTime;
+                    monitorData[functionName].minutesFromLastInvoke=minutesFromLastInvoke;
                 }
-                callback({ err: null, data: this.monitorData});
+                callback({ err: null, data: monitorData});
             }
         });
     }
